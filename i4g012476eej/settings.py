03e0dd069af192a3fcef6475bf316f1fbba117cf
Nems1 <nemwel@hotmail.com>
2022-06-24 20:59:16 +0300

    Create Project Model

--- i4g012476eej/settings.py
+++ i4g012476eej/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-from decouple import Config
+from decouple import config
 from typing import List
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = Config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS:list(str) = []
+ALLOWED_HOSTS: list[str] = []
 
 
 # Application definition
